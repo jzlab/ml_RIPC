7f91c9b052e6f533f4a500ea2a16f447070b5159
Elijah C <ejd.christensen@gmail.com>
2018-11-20 17:11:25 -0700

    working process daat

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ml_RIPC
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/jzlab/ml_RIPC/master?filepath=notebooks%2Fload_data.ipynb)
--- src/process_data.py
+++ src/process_data.py
@@ -16,7 +16,8 @@ def fetch_data(file_id,filename,proj_path='/home/elijahc/dev/ml_ripc'):
 
     return fp
 
-def process_data(fp):
+def process_data(fn,proj_path='/home/elijahc/dev/ml_ripc',baseline='A'):
+    fp = os.path.join(proj_path,fn)
     print('loading...'+fp)
     df = pd.read_csv(open(fp,mode='rb'),encoding='ISO-8859-1')
 
@@ -26,17 +27,18 @@ def process_data(fp):
 
     cols = list(df.columns)
 
-    rgx  = re.compile(r"\d{1,2}[A-L]")
+    rgx  = re.compile(r"(\d+)([A-L])")
 
     col_vec = list(filter(rgx.search,cols))
 
     long_df = pd.melt(df,id_vars=['Name','Formula','Molecular Weight'],value_vars=col_vec)
     pt_tp = list(long_df.variable)
-    tp_mat = np.array([[v[:-1],v[-1:]] for v in pt_tp])
+    tp_mat = np.array(list(map(rgx.split,pt_tp)))[:,1:3]
 
     long_df['timepoint']=tp_mat[:,1]
     long_df['pt']=tp_mat[:,0]
-    # t_lookup = {k:v for k,v in zip(np.unique(tp_mat[:,1],[0,1,2,3,4,6,8,10,20,30,45,60])}
+    t_lookup = {k:v for k,v in zip(np.unique(tp_mat[:,1]),[0,0,2,4,6,8,10,20,30,45,60,60])}
+    long_df['min']=[t_lookup[m] for m in list(long_df.timepoint)]
     # long_df['sec']=[t_lookup[m]*60 for m in list(long_df.timepoint)]
 
     return long_df
