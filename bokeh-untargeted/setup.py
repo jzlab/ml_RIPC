9b27e396c363186305309e3c5d8010de2be3d516
Elijah Christensen <ejd.christensen@gmail.com>
2019-01-07 14:16:17 -0700

    updated environment

--- bokeh-untargeted/setup.py
+++ bokeh-untargeted/setup.py
@@ -1,4 +1,12 @@
 import setuptools
 
 setuptools.setup(
-        )
+        name="jupyter-bokeh-server",
+        py_modules=['untargeted'],
+        entry_points={
+                'jupyter_serverproxy_servers':[
+                        'untargeted = untargeted:setup_untargeted'
+                ]
+        }
+)
+
